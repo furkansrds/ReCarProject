dff3e04c6590eb0ce2d6576366db89ff04a28146
Pikaa <furkanpikaa@gmail.com>
2023-11-09 13:56:11 +0300

    9.11.2023 #1

--- Core/DependencyResolvers/CoreModule.cs
+++ Core/DependencyResolvers/CoreModule.cs
@@ -1,4 +1,4 @@
-﻿using Core.Utilities.IoC;
+﻿using Core.Utilities.Security.IoC;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,12 +1,14 @@
 using Business.Abstract;
 using Business.Concrete;
 using Core.Utilities.Security.Encryption;
+using Core.Utilities.Security.IOC;
 using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -35,6 +37,8 @@ namespace WebAPI
         {
             services.AddControllers();
 
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -51,7 +55,7 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-
+            ServiceTool.Create(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
